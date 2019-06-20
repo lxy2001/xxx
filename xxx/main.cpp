2d6f41988c0ad58338a2ec5cfa53db5d4e891b87
lxy2001 <1771478860@qq.com>
2019-06-20 15:01:33 +0800

    Initial Commit

---
+++ xxx/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xxx
+//
+//  Created by s20181106279 on 2019/6/20.
+//  Copyright © 2019 s20181106279. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
